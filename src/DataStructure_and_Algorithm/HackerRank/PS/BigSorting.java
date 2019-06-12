ed24ab00bf61e4ff9ec6898867d7fc9675a77407
Vishnu T <vishnuthamiz96@gmail.com>
2019-06-12 07:57:37 +0530

    Time complexity

---
+++ src/DataStructure_and_Algorithm/HackerRank/PS/BigSorting.java
@@ -0,0 +1,37 @@
+package DataStructure_and_Algorithm.HackerRank.PS;
+
+
+import java.util.Scanner;
+
+public class BigSorting {
+
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int n = scan.nextInt();
+
+        String [] s = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            s[i] = scan.next();
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            int res = i;
+            for (int j = 1 + i; j < n; j++) {
+                 if (Double.parseDouble(s[res]) > Double.parseDouble(s[j]) ) {
+                      res = j;
+                 }
+            }
+
+            String ss = s[res];
+            s[res] = s[i]; // swapping the number
+            s[i] = ss;
+            System.out.println(s[i]);
+
+        }
+
+    }
+}
