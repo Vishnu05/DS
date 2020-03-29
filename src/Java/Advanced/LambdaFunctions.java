84b344ba4a1f2d71ff38ab736372b8a266e79b4a
= <vishnuthamiz96@gmail.com>
2020-03-29 20:46:32 +0530

    removing .class java files

---
+++ src/Java/Advanced/LambdaFunctions.java
@@ -0,0 +1,15 @@
+package Java.Advanced;
+
+import java.util.HashMap;
+
+public class LambdaFunctions {
+
+    public static void main(String args[]){
+
+        HashMap<Integer, String> hm = new HashMap();
+        hm.put(1, "Vishnu");
+        hm.put(2, "Sundar");
+        hm.put(3, "IT");
+
+    }
+}
--- yoo.java
+++
@@ -1,6 +0,0 @@
-
-gb hbjhb
-
-vidji
-
-
